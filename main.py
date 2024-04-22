340e12fd1998d26db0743c73dcc1a78308369b3b
teckenrod <treyeck2@gmail.com>
2024-04-22 09:29:17 -0500

    Added speed calculation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import requests
+import time
 
 # Set up firebase
 firebase_database_url = 'https://ssm-csu-default-rtdb.firebaseio.com/'
@@ -17,6 +18,11 @@ def send_to_firebase(data):
 
 # Open the webcam
 cam = cv2.VideoCapture(0)
+
+# Initialize variables
+prev_time = time.time()
+prev_x = 0
+prev_y = 0
 
 while True:
 	ret, frame = cam.read()
@@ -41,14 +47,25 @@ while True:
 	in_frame = False
 	x_data = 0
 	y_data = 0
+	radius_data = 0
+	speed = 0
+
+	largest_contour = None
+	largest_area = 0
 	for contour in contours:
 		area = cv2.contourArea(contour)
+		if area > largest_area:
+			largest_area = area
+			largest_contour = contour
+		
+		
+	if largest_contour is not None:
 		perimeter = cv2.arcLength(contour, True)
 		if perimeter != 0:
-			circularity = 4 * np.pi * area / (perimeter ** 2)
+			circularity = 4 * np.pi * largest_area / (perimeter ** 2)
 			
 			# You may need to adjust the circularity threshold based on your puck size and shape
-			if circularity > 0.7:
+			if circularity > 0.3:
 				# Draw a circle around the detected puck
 				(x, y), radius = cv2.minEnclosingCircle(contour)
 				center = (int(x), int(y))
@@ -57,15 +74,31 @@ while True:
 				in_frame = True
 				x_data = x
 				y_data = y
+				radius_data = radius
+
+	change_in_distance = sqrt((x_data-prev_x)^2+(y_data-prev_y)^2)
+	prev_x = x_data
+	prev_y = y_data
+
+	current_time_time = time.time()
+    time_difference = current_time - prev_time
+	prev_time = current_time
+
+	if radius_data != 0 {
+		speed = change_in_distance * (15/radius_data) / time_difference 
+	}
+
 	data = {
 		"in_frame": in_frame,
 		"x": x_data,
-		"y": y_data
+		"y": y_data,
+		"rad": radius_data
+		"speed": speed
 	}
 	send_to_firebase(data)
 
 	# Display the frame with detected puck
-	# cv2.imshow("Air Hockey Puck Detection", frame)
+	cv2.imshow("Air Hockey Puck Detection", frame)
     
 	key = cv2.waitKey(1) & 0xFF
 	if key == ord('q'):
